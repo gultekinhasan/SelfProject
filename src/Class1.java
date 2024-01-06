1f9d24a5e1b4ae2761ef5dc9b7420a0cbc8cc9b2
Hasan GULTEKIN <av.gultekinhasan@gmail.com>
2024-01-06 23:57:53 +0000

    -changes made in Class1

--- src/Class1.java
+++ src/Class1.java
@@ -2,7 +2,7 @@ public class Class1 {
     public static void main(String[] args) {
         System.out.println("Self Project Class-1, first line..");
         System.out.println("Self Project Class-1, 2. line.. (changed in branch) ");
-        System.out.println("Self Project Class-1, third line..");
+        System.out.println("Self Project Class-1, tenth line..");
         System.out.println("Self Project Class-1, fourth line, (added in branch) ");
 
     }
