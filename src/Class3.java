2270ab8c30fd5827dfb2e19142c10e5a31b45f55
Hasan Gultekin <146466391+gultekinhasan@users.noreply.github.com>
2024-01-06 23:30:07 +0000

    Update Class3.java from GitHub

--- src/Class3.java
+++ src/Class3.java
@@ -3,5 +3,7 @@ public class Class3 {
         System.out.println("Self Project Class-3, first line..");
         System.out.println("Self Project Class-3, second line..");
         System.out.println("Self Project Class-3, third line..");
+        System.out.println("Self Project Class-3, fourth line, (added in GitHub) ");
+        System.out.println("Self Project Class-3, fifth line,  (added in GitHub) ");
     }
 }
