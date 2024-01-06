314b8891d42b2f2a08a083b553833331dadeb22c
Hasan GULTEKIN <av.gultekinhasan@gmail.com>
2024-01-06 23:39:34 +0000

    -A few changes made in Class1, -RH10_3GP Class created by branch.

---
+++ src/RH10_3GP.java
@@ -0,0 +1,6 @@
+public class RH10_3GP {
+    public static void main(String[] args) {
+        System.out.println("Self Project, RH10_3GP Class in Branch, first line.. ");
+        System.out.println("Self Project, RH10_3GP Class in Branch, second line.. ");
+    }
+}
